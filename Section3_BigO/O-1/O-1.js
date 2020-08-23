19d02449c94a07030ea3821d837290d8c7e1646b
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-08-23 18:58:34 -0400

    added constant time note

--- Section3_BigO/O-1/O-1.js
+++ Section3_BigO/O-1/O-1.js
@@ -6,3 +6,5 @@ function compressFirstBox(boxes) {
     console.log(boxes[0]); // O(1)
     console.log(boxes[1]); // O(1)
 } // O(2) - Constant Time
+
+//If it is constant time, you round down to O(1) no matter the # of steps
