67f4c0f23c48858e669d8a5b05aa2cf87ec0ce02
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-08-23 18:56:46 -0400

    added constant time example

---
+++ Section3_BigO/O-1/O-1.js
@@ -0,0 +1,8 @@
+function compressFirstBox(boxes) {
+    console.log(boxes[0]);
+} // O(1) - Constant Time
+
+function compressFirstBox(boxes) {
+    console.log(boxes[0]); // O(1)
+    console.log(boxes[1]); // O(1)
+} // O(2) - Constant Time
