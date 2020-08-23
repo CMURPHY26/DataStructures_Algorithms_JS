467076cd63fb0b3a9c529bb6861c77316775cd78
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-08-23 18:50:11 -0400

    added linear time function

---
+++ Section3_BigO/O-n/O-n.js
@@ -0,0 +1,22 @@
+//#1 -- For loop in Javascript.
+const fish = ['dory', 'bruce', 'marlin', 'nemo'];
+const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+const large = new Array(1000).fill('nemo');
+
+function findNemo2(fish) {
+  let t0 = performance.now();
+  for (let i = 0; i < fish.length; i++) {
+    if (fish[i] === 'nemo') {
+      console.log('Found NEMO!');
+    }
+  }
+  let t1 = performance.now();
+  console.log("Call to find Nemo took " + (t1 - t0) + " milliseconds.");
+}
+
+findNemo2(large); // O(n) --> Linear Time --> Fair in Complexity Chart
+
+//If there was only one array item O(1);
+//If there were 10 array items O(10);
+
