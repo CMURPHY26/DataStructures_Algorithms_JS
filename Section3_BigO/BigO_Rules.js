265be1b600ec9a47c154526a9c97a36319eccbe0
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-08-29 13:50:21 -0400

    added BigO Rules with examples

--- Section3_BigO/BigO_Rules.js
+++ Section3_BigO/BigO_Rules.js
@@ -1,6 +1,9 @@
 /* 
 Rule Book
-Rule 1: Worst Case
+///////////////////////
+///Rule 1: Worst Case
+//////////////////////
+
 const nemo = ['nemo'];
 
 function findNemo1(array) {
@@ -17,8 +20,10 @@ findNemo1(nemo);
 Best case - nemo could be the first item in the array - O(1)
 Worst case - nemo could be the last item in the array - O(n)
 
+//////////////////////////////
+/////Rule 2: Remove Constants
+/////////////////////////////
 
-Rule 2: Remove Constants
 function printFirstItemThenFirstHalfThenSayHi100Times(items) {
     console.log(items[0]); // O(1)
 
@@ -51,8 +56,11 @@ function compressBoxesTwice(boxes) {
 
 BigO: O(2n) simplifies by dropping constants to O(n)
 
+///////////////////////////////////////
+/////Rule 3: Different terms for inputs
+////////////////////////////////////////
 
-Rule 3: Different terms for inputs
+
 function compressBoxesTwice(boxes,boxes2) {
     boxes.forEach(function(box) { // O(n)
         console.log(box)
@@ -67,7 +75,40 @@ BigO: O(n + m)
 //does not simplify to O(n) because the inputs are of separate terms. The first box array could only contain one element where the second array could contain 1000 elements.
 
 
-Rule 4: Drop Non Dominants
+function compressBoxesTwice(boxes,boxes2) {
+    boxes.forEach(function(box) { // O(n)
+        console.log(box)
+        boxes2.forEach(function(box) { // O(m)
+            console.log(box)
+        })
+    })
+} 
 
+//Separation of terms
+BigO(n * m)
+
+///////////////////////////////////
+//////Rule 4: Drop Non Dominants
+///////////////////////////////////
+
+function printAllNumbersThenAllPairSums(numbers) {
+
+  console.log('these are the numbers:');
+  numbers.forEach(function(number) { // O(n)
+    console.log(number);
+  });
+
+  console.log('and these are their sums:');
+  numbers.forEach(function(firstNumber) { //O(n^2)
+    numbers.forEach(function(secondNumber) {
+      console.log(firstNumber + secondNumber);
+    });
+  });
+}
+
+printAllNumbersThenAllPairSums([1,2,3,4,5])
+
+BigO: O(n + n^2) simplifies to O(n^2) by dropping non-dominant O(n)
+//as the input gets larger the size of O(n^2) is more important than O(n)
 
 */
